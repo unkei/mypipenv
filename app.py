e964fb8e50a5041e800d510edad8fde390a7c952
unkei <unkei@yahoo.co.jp>
2018-05-05 12:00:26 +0900

    added opencv sample.

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#-*- coding:utf-8 -*-
+import cv2
+import numpy as np
 
-print('hello world!')
+
+def main():
+    # 画像の読み込み(RGB)
+    img = cv2.imread("input.png")
+
+    # 画像の読み込み(グレースケール)
+    gray = cv2.imread("input.png", 0)
+
+    # 画像の読み込み(RGBA)
+    rgba = cv2.imread("input.png", -1)
+
+    # 画素値の表示
+    print("rgb=", img)
+    print("\n------------------------\n")
+    print("gray=", gray)
+    print("\n------------------------\n")
+    print("rgba=", rgba)
+
+
+if __name__ == "__main__":
+    main()
+
